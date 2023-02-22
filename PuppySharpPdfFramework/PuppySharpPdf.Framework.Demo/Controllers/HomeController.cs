3ac26b98ba5025d9b4f41351553a58ec7f8e732c
Mike Eckel <mike.eckel@education.ne.gov>
2023-02-21 20:21:45 -0600

    Generating pdf from static html file action added Controller actions seperated for razor and html file generation

--- PuppySharpPdfFramework/PuppySharpPdf.Framework.Demo/Controllers/HomeController.cs
+++ PuppySharpPdfFramework/PuppySharpPdf.Framework.Demo/Controllers/HomeController.cs
@@ -1,154 +1,169 @@
 ﻿using PuppySharpPdf.Framework.DemoProject.Models;
 using PuppySharpPdf.Framework.Renderers;
+using System;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 using Westwind.Web.Mvc;
 
 namespace PuppySharpPdf.Framework.DemoProject.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            return View();
-        }
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
 
-        public ActionResult GeneratePdfByUrl()
-        {
-            ViewBag.Message = "Your application description page.";
+		public ActionResult GeneratePdfByUrl()
+		{
+			ViewBag.Message = "Your application description page.";
 
-            return View(new PdfGenerationByUrlRequest());
-        }
+			return View(new PdfGenerationByUrlRequest());
+		}
 
-        [HttpPost]
-        public async Task<ActionResult> GeneratePdfByUrl(PdfGenerationByUrlRequest request)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(request);
-            }
+		[HttpPost]
+		public async Task<ActionResult> GeneratePdfByUrl(PdfGenerationByUrlRequest request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(request);
+			}
 
-            if (request.UseLocalExe)
-            {
-                var pdfRendererLocal = new PuppyPdfRenderer(options =>
-                {
-                    options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
+			if (request.UseLocalExe)
+			{
+				var pdfRendererLocal = new PuppyPdfRenderer(options =>
+				{
+					options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
 
-                });
+				});
 
-                var resultLocal = await pdfRendererLocal.GeneratePdfFromUrlAsync(request.Url);
+				var resultLocal = await pdfRendererLocal.GeneratePdfFromUrlAsync(request.Url);
 
-                return File(resultLocal, "application/pdf", "PdfFromUrl.pdf");
-            }
+				return File(resultLocal, "application/pdf", "PdfFromUrl.pdf");
+			}
 
-            var pdfRenderer = new PuppyPdfRenderer();
-            var result = await pdfRenderer.GeneratePdfFromUrlAsync(request.Url);
+			var pdfRenderer = new PuppyPdfRenderer();
+			var result = await pdfRenderer.GeneratePdfFromUrlAsync(request.Url);
 
-            return File(result, "application/pdf", "PdfFromUrl.pdf");
-        }
+			return File(result, "application/pdf", "PdfFromUrl.pdf");
+		}
 
-        public ActionResult GeneratePdfByUrlWithCustomOptions()
-        {
-            return View(new PdfGenerationByUrlRequest() { PdfOptions = new PdfOptionsViewModel() });
-        }
+		public ActionResult GeneratePdfByUrlWithCustomOptions()
+		{
+			return View(new PdfGenerationByUrlRequest() { PdfOptions = new PdfOptionsViewModel() });
+		}
 
-        [HttpPost]
-        public async Task<ActionResult> GeneratePdfByUrlWithCustomOptions(PdfGenerationByUrlRequest request)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(request);
-            }
+		[HttpPost]
+		public async Task<ActionResult> GeneratePdfByUrlWithCustomOptions(PdfGenerationByUrlRequest request)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(request);
+			}
 
-            if (request.UseLocalExe)
-            {
-                var pdfRendererLocal = new PuppyPdfRenderer(options =>
-                {
-                    options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
+			if (request.UseLocalExe)
+			{
+				var pdfRendererLocal = new PuppyPdfRenderer(options =>
+				{
+					options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
 
-                });
+				});
 
-                var resultLocal = await pdfRendererLocal.GeneratePdfFromUrlAsync(request.Url, options =>
-                {
-                    options.PrintBackground = request.PdfOptions.PrintBackground;
-                    options.Format = request.PdfOptions.GetPaperFormatType();
-                    options.Scale = request.PdfOptions.Scale;
-                    options.DisplayHeaderFooter = request.PdfOptions.DisplayHeaderFooter;
-                    options.HeaderTemplate = request.PdfOptions.HeaderTemplate;
-                    options.FooterTemplate = request.PdfOptions.FooterTemplate;
-                    options.Landscape = request.PdfOptions.Landscape;
-                    options.PageRanges = request.PdfOptions.PageRanges;
-                    options.Width = request.PdfOptions.Width;
-                    options.Height = request.PdfOptions.Height;
-                    options.MarginOptions = request.PdfOptions.MarginOptions;
-                    options.PreferCSSPageSize = request.PdfOptions.PreferCSSPageSize;
-                    options.OmitBackground = request.PdfOptions.OmitBackground;
-                });
+				var resultLocal = await pdfRendererLocal.GeneratePdfFromUrlAsync(request.Url, options =>
+				{
+					options.PrintBackground = request.PdfOptions.PrintBackground;
+					options.Format = request.PdfOptions.GetPaperFormatType();
+					options.Scale = request.PdfOptions.Scale;
+					options.DisplayHeaderFooter = request.PdfOptions.DisplayHeaderFooter;
+					options.HeaderTemplate = request.PdfOptions.HeaderTemplate;
+					options.FooterTemplate = request.PdfOptions.FooterTemplate;
+					options.Landscape = request.PdfOptions.Landscape;
+					options.PageRanges = request.PdfOptions.PageRanges;
+					options.Width = request.PdfOptions.Width;
+					options.Height = request.PdfOptions.Height;
+					options.MarginOptions = request.PdfOptions.MarginOptions;
+					options.PreferCSSPageSize = request.PdfOptions.PreferCSSPageSize;
+					options.OmitBackground = request.PdfOptions.OmitBackground;
+				});
 
-                return File(resultLocal, "application/pdf", "PdfFromUrlWithCustomOptions.pdf");
-            }
+				return File(resultLocal, "application/pdf", "PdfFromUrlWithCustomOptions.pdf");
+			}
 
-            var pdfRenderer = new PuppyPdfRenderer();
-            var result = await pdfRenderer.GeneratePdfFromUrlAsync(request.Url, options =>
-            {
-                options.PrintBackground = request.PdfOptions.PrintBackground;
-                options.Format = request.PdfOptions.GetPaperFormatType();
-                options.Scale = request.PdfOptions.Scale;
-                options.DisplayHeaderFooter = request.PdfOptions.DisplayHeaderFooter;
-                options.HeaderTemplate = request.PdfOptions.HeaderTemplate;
-                options.FooterTemplate = request.PdfOptions.FooterTemplate;
-                options.Landscape = request.PdfOptions.Landscape;
-                options.PageRanges = request.PdfOptions.PageRanges;
-                options.Width = request.PdfOptions.Width;
-                options.Height = request.PdfOptions.Height;
-                options.MarginOptions = request.PdfOptions.MarginOptions;
-                options.PreferCSSPageSize = request.PdfOptions.PreferCSSPageSize;
-                options.OmitBackground = request.PdfOptions.OmitBackground;
-            });
+			var pdfRenderer = new PuppyPdfRenderer();
+			var result = await pdfRenderer.GeneratePdfFromUrlAsync(request.Url, options =>
+			{
+				options.PrintBackground = request.PdfOptions.PrintBackground;
+				options.Format = request.PdfOptions.GetPaperFormatType();
+				options.Scale = request.PdfOptions.Scale;
+				options.DisplayHeaderFooter = request.PdfOptions.DisplayHeaderFooter;
+				options.HeaderTemplate = request.PdfOptions.HeaderTemplate;
+				options.FooterTemplate = request.PdfOptions.FooterTemplate;
+				options.Landscape = request.PdfOptions.Landscape;
+				options.PageRanges = request.PdfOptions.PageRanges;
+				options.Width = request.PdfOptions.Width;
+				options.Height = request.PdfOptions.Height;
+				options.MarginOptions = request.PdfOptions.MarginOptions;
+				options.PreferCSSPageSize = request.PdfOptions.PreferCSSPageSize;
+				options.OmitBackground = request.PdfOptions.OmitBackground;
+			});
 
-            return File(result, "application/pdf", "PdfFromUrlWithCustomOptions.pdf");
-        }
-
-
-        public ActionResult GeneratePdfUsingHtml()
-        {
+			return File(result, "application/pdf", "PdfFromUrlWithCustomOptions.pdf");
+		}
 
 
-            return View(new DemoTemplateViewModel());
-        }
+		public ActionResult GeneratePdfUsingHtml()
+		{
 
-        [HttpPost]
-        public async Task<ActionResult> GeneratePdfUsingHtml(DemoTemplateViewModel model)
-        {
-            if (!ModelState.IsValid)
-            {
-                return View(model);
-            }
 
-            var html = ViewRenderer.RenderPartialView("~/Views/Shared/Templates/Demo.cshtml", model);
+			return View(new DemoTemplateViewModel());
+		}
 
-            if (model.UseLocalExe)
-            {
-                var pdfRendererLocal = new PuppyPdfRenderer(options =>
-                {
-                    options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
+		[HttpPost]
+		public async Task<ActionResult> GeneratePdfUsingRazorTemplate(DemoTemplateViewModel model)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(model);
+			}
 
-                });
+			var html = ViewRenderer.RenderPartialView("~/Views/Shared/Templates/Demo.cshtml", model);
 
-                var resultLocal = await pdfRendererLocal.GeneratePdfFromHtmlAsync(html);
+			if (model.UseLocalExe)
+			{
+				var pdfRendererLocal = new PuppyPdfRenderer(options =>
+				{
+					options.ChromeExecutablePath = Server.MapPath("~/Content/ChromeBrowser/chrome-win/chrome.exe");
 
-                return File(resultLocal, "application/pdf", "PdfFromUrl.pdf");
-            }
+				});
 
-            var pdfRenderer = new PuppyPdfRenderer();
-            var result = await pdfRenderer.GeneratePdfFromHtmlAsync(html);
+				var resultLocal = await pdfRendererLocal.GeneratePdfFromHtmlAsync(html);
 
-            return File(result, "application/pdf", "GeneratedFromHtmlString.pdf");
-        }
+				return File(resultLocal, "application/pdf", "PdfFromUrl.pdf");
+			}
 
-        public ActionResult GeneratePdfUsingHtmlWithCustomOptions()
-        {
-            return View();
-        }
-    }
+			var pdfRenderer = new PuppyPdfRenderer();
+			var result = await pdfRenderer.GeneratePdfFromHtmlAsync(html);
+
+			return File(result, "application/pdf", "GeneratedFromRazorFile.pdf");
+		}
+
+
+		public async Task<ActionResult> GeneratePdfUsingHtmlTemplate()
+		{
+
+
+			var html = System.IO.File.ReadAllText(AppDomain.CurrentDomain.BaseDirectory + "Views/Shared/Templates/Demo.html");
+
+
+			var pdfRenderer = new PuppyPdfRenderer();
+			var result = await pdfRenderer.GeneratePdfFromHtmlAsync(html);
+
+			return File(result, "application/pdf", "GeneratedFromHtmlFile.pdf");
+		}
+
+		public ActionResult GeneratePdfUsingHtmlWithCustomOptions()
+		{
+			return View();
+		}
+	}
 }
